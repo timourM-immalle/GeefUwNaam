7fd853411e9c97d9ecd5526f034b1535cdf125df
timourM-immalle <timour.meeusen@immalle.eu>
2018-02-08 21:38:40 +0100

    begin

---
+++ naamontvangen.php
@@ -0,0 +1,5 @@
+<?php
+echo '<h2>' . "Bedankt voor het ingeven van uw naam!" . '</h2>';
+
+setcookie('naam');
+header('Location: C:\Users\Timour\Documents\Documenten\school\SO\IT\Softwarepakketten- en ontwikkeling\GeefUwNaam.html');
